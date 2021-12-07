69300522e3587dcb7332db56587f5489abba09be
milan.forrat <milan.forrat@gmail.com>
2021-12-07 15:25:43 +0100

    feat: wip touch compatibility

--- app.js
+++ app.js
@@ -4,8 +4,43 @@ window.onload = () => {
     document.querySelectorAll('#palette div').forEach(element => {
         // on parcourt les couleurs mises en HTML via les datasets et on les applique avec le DOM
         element.style.backgroundColor = element.dataset.color;
+        element.addEventListener('click', () => {
+            canvas.setColor(element.dataset.color);
+            canvas.insertCouleur(element.dataset.color);
+        })
+        element.addEventListener("mouseover", ()=>{
+            element.style.cursor ="pointer";
+        })
+    })
+    // chargement du canvas avec comme paramètre le nom du canva HTML qui intégrera ce dessin
+    let canvas = new Dessin('#feuille', 400, 400);
+
+    document.querySelector('#plus').addEventListener("click", () =>{
+        canvas.biggerStroke();
+        canvas.insertTaille(canvas.ctx.lineWidth);
+    })
+    document.querySelector('#plus').addEventListener("mouseover", ()=>{
+        document.querySelector('#plus').style.cursor ="pointer";
+    })
+    document.querySelector('#moins').addEventListener("click", () =>{
+        canvas.smallerStroke();
+        canvas.insertTaille(canvas.ctx.lineWidth);
+    })
+    document.querySelector('#moins').addEventListener("mouseover", ()=>{
+        document.querySelector('#moins').style.cursor ="pointer";
+    })
+    document.querySelector('#gomme').addEventListener("click", () =>{
+        canvas.setColor("white");
+        canvas.insertOutilGomme("Gomme", canvas.ctx.lineWidth);
+    })
+    document.querySelector('#gomme').addEventListener("mouseover", ()=>{
+        document.querySelector('#gomme').style.cursor ="pointer";
+    })
+    document.querySelector('#effacer').addEventListener("click", () =>{
+        canvas.erase();
+    })
+    document.querySelector('#effacer').addEventListener("mouseover", ()=>{
+        document.querySelector('#effacer').style.cursor ="pointer";
     })
 
-    // chargement du canvas avec comme paramètre le nom du canva HTML qui intégrera ce dessin
-    let canvas = new Dessin('#feuille');
 }
--- draw.js
+++ draw.js
@@ -1,13 +1,110 @@
 class Dessin{
     // constructor car on récupère le paramètre envoyé par app.js qui correspond au canvas HTML
-    constructor(canvas){
+    constructor(canvas, canvasWidth, canvasHeight){
         // suis-je entrain de dessiner -> non
         this.draw = false;
         this.prevX = 0;
         this.prevY = 0;
-
+        this.canvasWidth = 400;
+        this.canvasHeight = 400;
         this.canvas = document.querySelector(canvas);
         // je vérifie que je récupère bien le canvas HTML
         // console.log(this.canvas);
+        this.ctx = this.canvas.getContext("2d");
+        this.ctx.strokeStyle = "black";
+        this.ctx.lineWidth = 2;
+
+        this.canvas.addEventListener('mousedown', (e) =>{
+            this.draw = true;
+
+            this.prevX = (e.clientX - this.canvas.offsetLeft) * canvasWidth / this.canvas.clientWidth;
+            this.prevY = (e.clientY - this.canvas.offsetTop) * canvasHeight / this.canvas.clientHeight;
+        })
+
+        this.canvas.addEventListener('touchstart', (e) =>{
+            this.draw = true;
+
+            this.prevX = (e.clientX - this.canvas.offsetLeft) * canvasWidth / this.canvas.clientWidth;
+            this.prevY = (e.clientY - this.canvas.offsetTop) * canvasHeight / this.canvas.clientHeight;
+        })
+
+
+        this.canvas.addEventListener('mousemove', (e) =>{
+            if(this.draw){
+                let currX = (e.clientX - this.canvas.offsetLeft) * canvasWidth / this.canvas.clientWidth;
+                let currY = (e.clientY - this.canvas.offsetTop) * canvasHeight / this.canvas.clientHeight;
+
+                this.dessine(this.prevX, this.prevY, currX, currY);
+
+                this.prevX = currX;
+                this.prevY = currY;
+            }
+        })
+
+        this.canvas.addEventListener('touchmove', (e) =>{
+            if(this.draw){
+                let currX = (e.clientX - this.canvas.offsetLeft) * canvasWidth / this.canvas.clientWidth;
+                let currY = (e.clientY - this.canvas.offsetTop) * canvasHeight / this.canvas.clientHeight;
+
+                this.dessine(this.prevX, this.prevY, currX, currY);
+
+                this.prevX = currX;
+                this.prevY = currY;
+            }
+        })
+
+        this.canvas.addEventListener('mouseup', () => {
+            this.draw = false;
+        })
+
+        this.canvas.addEventListener('mouseout', () => {
+            this.draw = false;
+        })
     }
+
+    dessine(depX, depY, destX, destY){
+        this.ctx.beginPath();
+        this.ctx.moveTo(depX, depY);
+        this.ctx.lineTo(destX, destY);
+        this.ctx.closePath();
+        this.ctx.stroke();
+    }
+    
+    setColor(color){
+        this.ctx.strokeStyle = color;
+    }
+
+    insertCouleur(couleur){
+        let choixCouleur = document.getElementById('choix-outils');
+        choixCouleur.innerHTML = "Pinceau de couleur : <div style='display: inline-block; margin-bottom: -3px; border: 2px solid black; width: 20px; height: 15px; background-color:"+couleur+"'></div>";
+        this.canvas.style.cursor ="url('./pointer/paint.svg') 0 40, auto";
+    }
+
+
+    insertTaille(taille){
+        let choixTaille = document.getElementById('choix-taille');
+        choixTaille.innerHTML = "Taille sélectionnée : "+taille;
+    }
+
+    insertOutilGomme(outil){
+        let choixOutil = document.getElementById('choix-outils');
+        choixOutil.innerHTML = "Vous avez choisi l'outil : "+outil;
+        this.canvas.style.cursor ="url('./pointer/eraser.svg') 0 20, auto";
+    }
+
+    biggerStroke(){
+        this.ctx.lineWidth++;
+    }
+
+    smallerStroke(){
+        this.ctx.lineWidth = (this.ctx.lineWidth > 1) ? this.ctx.lineWidth -1 : 1; 
+    }
+
+    erase(){
+        if(confirm("Voulez vous vraiment effacer votre dessin ?")){
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+    }
+
+
 }
